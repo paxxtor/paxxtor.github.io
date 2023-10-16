4575eb33b688d82733c54c30c91c02abfdd3af76
Miguel Paxtor <93008981+paxxtor@users.noreply.github.com>
2023-10-15 22:26:14 -0600

    Add files via upload
    Proyecto

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+const btnHamburgerMenu = document.querySelector('#btn_hamburger__menu')
+const headerMenuNavbar = document.querySelector('.header-menu-navbar')
+const navMenu = document.querySelectorAll('.nav-menu')
+const copyright = document.querySelector('.copyright')
+
+btnHamburgerMenu.addEventListener('click', () => {
+  headerMenuNavbar.classList.toggle('show-menu')
+})
+
+navMenu.forEach(menu => {
+  menu.addEventListener('click', () => {
+    headerMenuNavbar.classList.remove('show-menu')
+  })
+})
+
+const year = new Date()
+copyright.textContent = `© ${year.getFullYear()} Miguel Paxtor | Frontend Developer`
